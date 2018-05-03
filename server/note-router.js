d1bae5f9e9051e66545ebfa36718ee48daa98fd6
Yijiao Wang <ttwyj250@gmail.com>
2018-05-03 00:33:47 -0700

    Add server route to update note given its objectID string #5

--- server/note-router.js
+++ server/note-router.js
@@ -1,14 +1,24 @@
 const { Router } = require('express')
-const NoteSchema = require('./models/NoteSchema')
+const Note = require('./models/NoteSchema')
 
 module.exports = function noteRouter() {
   const router = new Router()
   router.post('/new', async (req, res) => {
-    const newNote = NoteSchema(req.body)
-    console.log(newNote)
     try {
-      const savedNote = await newNote.save()
-      res.status(201).send(savedNote)
+      const noteSaved = await Note.create(req.body)
+      res.status(201).send(noteSaved)
+    }
+    catch (err) {
+      console.error(err)
+      res.status(500).json({ error: 'Internal Server Error' })
+    }
+  })
+  router.post('/edit/:id/', async (req, res) => {
+    try {
+      const noteUpdated = await Note.findByIdAndUpdate(req.params.id, {
+        $set: req.body
+      }).exec()
+      res.status(200).json(noteUpdated)
     }
     catch (err) {
       console.error(err)
