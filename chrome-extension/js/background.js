3fc19390756237f0048e7d8c64653c113677980f
Yijiao Wang <ttwyj250@gmail.com>
2018-05-03 15:16:30 -0700

    Programmatically inject css files for content scripts into new tab #5

--- chrome-extension/js/background.js
+++ chrome-extension/js/background.js
@@ -5,6 +5,8 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.from === 'popup_script' && message.action === 'createTab') {
     currentUser = message.currentUser
     chrome.tabs.create({ url: message.url }, tab => {
+      chrome.tabs.insertCSS(tab.id, { file: 'css/bootstrap.min.css' })
+      chrome.tabs.insertCSS(tab.id, { file: 'css/content.css' })
       chrome.tabs.executeScript(tab.id, { file: 'js/content_script_bundle.js' })
     })
   }
