b54cdc203c574a90cd569953a2e74a51a4aa4e44
Yijiao Wang <ttwyj250@gmail.com>
2018-05-03 14:41:58 -0700

    Update Note model to include article_id and article_link #5

--- server/models/NoteSchema.js
+++ server/models/NoteSchema.js
@@ -5,7 +5,8 @@ const NoteSchema = new Schema({
   title: { type: String, default: 'Untitled' },
   content: { type: String, required: true },
   author_id: { type: String, required: true },
-  article_link: String
+  article_id: { type: String, required: true },
+  article_link: { type: String, required: true }
 })
 
 module.exports = mongoose.model('Note', NoteSchema)
