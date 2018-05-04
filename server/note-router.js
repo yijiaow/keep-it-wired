9635bd69c07525889eaf12ef32a3c61c23fb7309
Yijiao Wang <ttwyj250@gmail.com>
2018-05-04 10:28:05 -0700

    Add server routes to get and delete notes #6

--- server/note-router.js
+++ server/note-router.js
@@ -3,6 +3,16 @@ const Note = require('./models/NoteSchema')
 
 module.exports = function noteRouter() {
   const router = new Router()
+  router.get('/', async (req, res) => {
+    try {
+      const notes = await Note.find({ author_id: req.query.user })
+      res.status(200).send(notes)
+    }
+    catch (err) {
+      console.error(err)
+      res.status(500).send(err)
+    }
+  })
   router.post('/new', async (req, res) => {
     try {
       const noteSaved = await Note.create(req.body)
@@ -22,7 +32,16 @@ module.exports = function noteRouter() {
     }
     catch (err) {
       console.error(err)
-      res.status(500).json({ error: 'Internal Server Error' })
+      res.status(500).send(err)
+    }
+  })
+  router.delete('/delete/:id', async (req, res) => {
+    try {
+      await Note.findByIdAndRemove(req.params.id)
+      res.status(200).send('Note successfully deleted].')
+    }
+    catch (err) {
+      res.status(500).send(err)
     }
   })
   return router
