8b0984863e9f320ad7a6143f4267cc13a9fa8852
Yijiao Wang <ttwyj250@gmail.com>
2018-05-05 18:21:20 -0700

    Use PureComponent to avoid wasteful re-renders on hover #6

--- client/notes.js
+++ client/notes.js
@@ -1,12 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component, PureComponent } from 'react'
 
+class Note extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = { hovered: false }
+    this.handleHover = this.handleHover.bind(this)
+    this.handleDelete = this.handleDelete.bind(this)
+  }
+  handleHover() {
+    this.setState({ hovered: !this.state.hovered })
+  }
+  handleDelete(event) {
+    const { id, index } = event.currentTarget.closest('.card').dataset
+    this.props.handleDeleteNote(id, index)
+  }
+  render() {
+    return (
+      <div
+        className="card"
+        data-id={this.props.note._id}
+        data-index={this.props.index}
+        onMouseEnter={this.handleHover}
+        onMouseLeave={this.handleHover}
+      >
+        <div className="card-body">
+          <h5 className="card-title text-highlight">{this.props.note.title}</h5>
+          <p className="card-text">{this.props.note.content}</p>
+          {this.state.hovered && (
+            <button className="btn" onClick={this.handleDelete}>
+              Delete
+            </button>
+          )}
+        </div>
+      </div>
+    )
+  }
+}
 export class Notes extends Component {
   constructor(props) {
     super(props)
     this.state = { active: null }
-    this.handleHover = this.handleHover.bind(this)
     this.handleDeleteNote = this.handleDeleteNote.bind(this)
-    this.renderNoteCard = this.renderNoteCard.bind(this)
   }
   componentDidMount() {
     fetch(`http://127.0.0.1:3000/note?user=${this.props.currentUser.userId}`)
@@ -18,50 +52,36 @@ export class Notes extends Component {
         this.setState({ error: err })
       })
   }
-  handleHover(event) {
-    this.setState({ active: event.currentTarget.dataset.id })
-  }
-  handleDeleteNote(event) {
-    const { id, index } = event.currentTarget.closest('.card').dataset
-    const updatedNoteList = this.state.notes
-      .slice(0, index)
-      .concat(this.state.notes.slice(index + 1))
+  handleDeleteNote(id, index) {
     fetch(`http://127.0.0.1:3000/note/delete/${id}`, {
       method: 'DELETE'
     })
-      .then(this.setState({ notes: updatedNoteList }))
+      .then(
+        this.setState({
+          notes: [
+            ...this.state.notes.slice(0, index),
+            ...this.state.notes.slice(index + 1)
+          ]
+        })
+      )
       .catch(err => {
         this.setState({ error: err })
       })
   }
-  renderNoteCard(note, key) {
-    const $deleteBtn = note._id === this.state.active && (
-      <button className="btn" onClick={this.handleDeleteNote}>
-        Delete
-      </button>
-    )
-    return (
-      <div
-        className="card"
-        key={key}
-        data-index={key}
-        data-id={note._id}
-        onMouseOver={this.handleHover}
-      >
-        <div className="card-body">
-          <h5 className="card-title text-highlight">{note.title}</h5>
-          <p className="card-text">{note.content}</p>
-          {$deleteBtn}
-        </div>
-      </div>
-    )
-  }
+
   render() {
     if (this.state.error) throw new Error(this.state.error)
     else if (this.state.notes && this.state.notes.length > 0) {
       return (
         <div className="notes-view d-flex flex-wrap justify-content-around align-content-between">
-          {this.state.notes.map(this.renderNoteCard)}
+          {this.state.notes.map((note, index) => (
+            <Note
+              key={index}
+              index={index}
+              note={note}
+              handleDeleteNote={this.handleDeleteNote}
+            />
+          ))}
         </div>
       )
     }
